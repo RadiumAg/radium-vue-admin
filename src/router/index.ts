8996e2b35defc09e82e201eb7a4b33843ad6b113
Radium <2043869032@qq.com>
2022-05-01 17:52:54 +0800

    commit

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import Layout from "@pages/layout/layout.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+export  const router = createRouter({
+    history:createWebHistory(),
+    routes:[{
+        path:'',
+         component:Layout,
+    }]
+});
