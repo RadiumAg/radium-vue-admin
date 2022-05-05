e340fbe3ca12a62aaa4efbff4acb9a9426b222a3
Radium <2043869032@qq.com>
2022-05-05 20:32:14 +0800

    commit

--- admin-backend/src/main.ts
+++ admin-backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-
+import * as compression from 'compression';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
