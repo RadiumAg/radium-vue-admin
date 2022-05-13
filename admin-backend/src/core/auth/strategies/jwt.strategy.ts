d5ce1a66c11abaf9cd0d7ddc57919b15044ee91b
Radium <2043869032@qq.com>
2022-05-13 18:27:40 +0800

    提交

---
+++ admin-backend/src/core/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from '../constants';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: (request: Request) => {
+                console.log(request);
+            },
+            ignoreExpiration: false,
+            secretOrKey: jwtConstants.secret,
+        });
+    }
+
+    async validate(payload: any) {
+        return { userId: payload.sub, username: payload.username };
+    }
+}
